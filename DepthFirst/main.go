fd31594386aebd37067398d697f1a41393b6dddf
sleepynut <nut.pa@kbtg.tech>
2022-10-31 19:39:05 +0700

    hasPath Problem
    Depth First search
    
    - implement search through iteration
    - implement search though recursive

--- DepthFirst/main.go
+++ DepthFirst/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	adj := map[string][]string{
 		"a": {"c", "b"},
@@ -12,4 +14,43 @@ func main() {
 
 	dfs("a", adj)
 	dfsRe("a", adj)
+
+	src := "a"
+	dst := "a"
+	fmt.Printf("\nHas Path from %s - %s: %t\n", src, dst, hasPath(adj, src, dst))
+	fmt.Printf("\nHas Path (Re) from %s - %s: %t\n", src, dst, hasPathRe(adj, src, dst))
+}
+
+func hasPath(graph map[string][]string, src string, dst string) bool {
+	var s Stack
+	var v string
+	var b bool
+	s.Push(src)
+
+	for {
+		v, b = s.Pop()
+		if !b || v == dst {
+			break
+		}
+
+		for _, node := range graph[v] {
+			s.Push(node)
+		}
+	}
+
+	return v == dst
+}
+
+func hasPathRe(graph map[string][]string, src string, dst string) bool {
+	if src == dst {
+		return true
+	}
+
+	for _, neighbor := range graph[src] {
+		if hasPathRe(graph, neighbor, dst) {
+			return true
+		}
+	}
+
+	return false
 }
